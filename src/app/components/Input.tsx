cfd9e670b4fc840052beb005ed2e81c59eed9445
Yaba Ernesto <yabaernesto@gmail.com>
2024-11-26 16:52:14 +0100

    feat: Added form animations

--- src/app/components/Input.tsx
+++ src/app/components/Input.tsx
@@ -23,7 +23,7 @@ export type RootProps = ComponentProps<'div'>
 export function Root(props: RootProps) {
   return (
     <div 
-      className="mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm" 
+      className="mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm focus-within:border-violet-300 focus-within:ring-4 focus-within:ring-violet-100" 
       {...props}  
     />
   )
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -21,6 +21,23 @@ export default {
           25: '#fcfaff',
         },
       },
+
+      keyframes: {
+        slideDownAndFade: {
+          from: {
+            opacity: '0', 
+            transform: 'translateY(-2px)' 
+          },
+          to: {
+            opacity: '1', 
+            transform: 'translateY(0)' 
+          },
+        },
+      },
+
+      animation: {
+        slideDownAndFade: 'slideDownAndFade 400ms cubic-bezier(0.16, 0.3, 1)',
+      },
     },
   },
   plugins: [],
